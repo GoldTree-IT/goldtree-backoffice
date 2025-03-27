34fcd46263aca3681c71e55dfc60e62426769672
Anjesh.Ghimire <anjesh.ghimire@globalsquareit.com>
2025-03-27 21:49:48 +0545

    chore: clean up files and creating routes

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Welcome to the home page!</p>
+      <p>Here is some content.</p>
+    </>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+
+    <html lang="ja">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+
+  );
+}
